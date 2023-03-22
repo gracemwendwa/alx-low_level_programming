a6b945d3569835efc7d84a35f470abaea239b68e
gracemwendwa <mwendwagracie@gmail.com>
2023-03-22 07:06:09 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
- */
-
+* main - prints putchar
+* Return: always 0
+*/
 int main(void)
-
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
 
+	return (0);
 }

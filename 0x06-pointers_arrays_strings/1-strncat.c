237c1b95569cd44b89df7e0fc75d4bcdfdd7be1c
gracemwendwa <mwendwagracie@gmail.com>
2023-03-29 23:02:52 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+Executable File  32 lines (26 sloc)  534 Bytes
+
+#include "main.h"
+/**
+ *_strncat -  concatenates two strings
+ *@dest: A pointer to an char that will be updated
+ *@src: A pointer to an char that will be updated
+ *@n: value
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0') /*WHILE count character */
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0') /*WHILE concatenate dest with src*/
+	{
+		dest[i] = src[j];
+		j++;  /*sum j*/
+		i++; /**sum i*/
+	}
+
+	dest[i] = '\0'; /*the terminating null byte*/
+
+	return (dest);
+}
